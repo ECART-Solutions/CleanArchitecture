6d50a1adb1c2dac9d612c2a6736e76885d704c57
Daniel WOAGOU <pwoagou@otr.tg>
2022-12-30 12:05:30 +0000

    Creating Result message manager

---
+++ src/Client/Shared/ResultMessageManager.cs
@@ -0,0 +1,43 @@
+﻿using BlazorHero.CleanArchitecture.Shared.Wrapper;
+
+using MudBlazor;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Shared
+{
+    public static class ResultMessageManager
+    {
+        public static void ManageResult(this IResult result, ISnackbar snackBar, Func<Task> succeededAction = null, Func<Task> failedAction = null)
+        {
+            try
+            {
+                if (result.Succeeded)
+                {
+                    if (result.Messages.Any())
+                    {
+                        snackBar.Add(result.Messages[0], Severity.Success);
+                    }
+                    if(succeededAction is not null)
+                    Task.Run(succeededAction).AndForget(TaskOption.Safe);
+                }
+                else
+                {
+                    foreach (var message in result.Messages)
+                    {
+                        snackBar.Add(message, Severity.Error);                        
+                    }
+                    if (failedAction is not null)
+                        Task.Run(failedAction);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }            
+        }
+
+    }
+}
